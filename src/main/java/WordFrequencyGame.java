b176ce5ac0f232be1471a4062ce5af8e66ba94f0
Arianne <chiescake11@gmail.com>
2023-08-21 19:01:06 +0800

    refactor: create getWordFrequencyInfosList method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -16,32 +16,37 @@ public class WordFrequencyGame {
             return inputStr + " 1";
         } else {
             try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = inputStr.split(SPACE_DELIMITER);
-
-                List<WordFrequencyInfo> wordFrequencyInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(word, 1);
-                    wordFrequencyInfoList.add(wordFrequencyInfo);
-                }
-
-                //get the wordFrequencyMap for the next step of sizing the same word
-                Map<String, List<WordFrequencyInfo>> wordFrequencyMap = getListMap(wordFrequencyInfoList);
-
-                List<WordFrequencyInfo> frequencyInfos = new ArrayList<>();
-                for (Map.Entry<String, List<WordFrequencyInfo>> entry : wordFrequencyMap.entrySet()) {
-                    WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(entry.getKey(), entry.getValue().size());
-                    frequencyInfos.add(wordFrequencyInfo);
-                }
-                wordFrequencyInfoList = frequencyInfos;
-
-                wordFrequencyInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
+                List<WordFrequencyInfo> wordFrequencyInfoList = getWordFrequencyInfosList(inputStr);
 
                 return generatePrintLines(wordFrequencyInfoList);
             } catch (Exception e) {
                 return CALCULATE_ERROR;
             }
         }
+    }
+
+    private List<WordFrequencyInfo> getWordFrequencyInfosList(String inputStr) {
+        //split the input string with 1 to n pieces of spaces
+        String[] words = inputStr.split(SPACE_DELIMITER);
+
+        List<WordFrequencyInfo> wordFrequencyInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(word, 1);
+            wordFrequencyInfoList.add(wordFrequencyInfo);
+        }
+
+        //get the wordFrequencyMap for the next step of sizing the same word
+        Map<String, List<WordFrequencyInfo>> wordFrequencyMap = getListMap(wordFrequencyInfoList);
+
+        List<WordFrequencyInfo> frequencyInfos = new ArrayList<>();
+        for (Map.Entry<String, List<WordFrequencyInfo>> entry : wordFrequencyMap.entrySet()) {
+            WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(entry.getKey(), entry.getValue().size());
+            frequencyInfos.add(wordFrequencyInfo);
+        }
+        wordFrequencyInfoList = frequencyInfos;
+
+        wordFrequencyInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
+        return wordFrequencyInfoList;
     }
 
     private static String generatePrintLines(List<WordFrequencyInfo> wordFrequencyInfoList) {
